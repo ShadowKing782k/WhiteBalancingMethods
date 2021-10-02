0b1f2624188bb5b9b12950f79fc6c4deedddb50b
Tejesh ANS <tejeshans782k@gmail.com>
2021-10-02 09:42:27 +0530

    Third Commit

--- UnderWaterImageEnhancement.py
+++ UnderWaterImageEnhancement.py
@@ -1,5 +1,6 @@
 from skimage.io import imread, imshow
-from skimage.util import img_as_ubyte, img_as_float
+from skimage.util import img_as_ubyte, img_as_float, img_as_uint
+import skimage.filters
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -33,10 +34,10 @@ def redChannelCompnesation(image):
     floatImage = img_as_float(image)
     #print(floatImage.shape[0])
     channelMeans  = floatImage.mean(axis=(0, 1))
-    # print(channelMeans)
+    print(channelMeans)
     redChannelCompensatedImage = floatImage
     # print(1 - redChannelCompensatedImage[0])
-    print(redChannelCompensatedImage[0].shape)
+   #  print(redChannelCompensatedImage[0].shape)
 
 
     for i in range(0, redChannelCompensatedImage.shape[0]):
@@ -47,38 +48,37 @@ def redChannelCompnesation(image):
                                                   1*(channelMeans[1] - channelMeans[0])*(1 - redChannelCompensatedImage[i][j][0])*redChannelCompensatedImage[i][j][1]
 
 
-   # print(redChannelCompensatedImage.mean(axis=(0, 1)))
+    print(redChannelCompensatedImage.mean(axis=(0, 1)))
     return img_as_ubyte(redChannelCompensatedImage)
 
 
-def gammaCorrection(image):
-    floatImage = img_as_float(image)
-    return exposure.adjust_gamma(image, 2.5)
+def gammaCorrection(image, gamma=1.0):
+    return (image/255)**(gamma)
+
 
 image = imread("images.jfif")
-# print(image)
-fig, ax = plt.subplots(1, 2)
+redChannelCompnesatedImage = redChannelCompnesation(image)
+grayWorldImage = gray_world(redChannelCompnesatedImage)
+# print(grayWorldImage)
+# print(floatgrayWorldImage)
+
+# print(grayWorldImage.shape)
+#
+# floatgrayWorldImage = img_as_float(grayWorldImage)
+# print(floatgrayWorldImage.mean(axis=(0, 1)))
+# print(img_as_float(image).mean(axis=(0, 1)))
 
 
-ax[0].imshow(image)
-ax[0].set_title("Orginal Image")
-ax[1].imshow(whitePatchAlgorithm(image, 95))
-ax[1].set_title("WhitePatch Image")
+fig0, ax0 = plt.subplots(3, 3, sharex='all', sharey='all')
 
-fig1, ax1 = plt.subplots(1, 2)
-
-ax1[0].imshow(gray_world(image))
-ax1[0].set_title("Gray World Image")
-ax1[1].imshow(gray_world(gammaCorrection(redChannelCompnesation(image))))
-ax1[1].set_title("Gray World Image(RCC + GC)")
-
-fig2, ax2 = plt.subplots(1, 2)
-
-ax2[0].imshow(gray_world(redChannelCompnesation(image)))
-ax2[0].set_title("Gray World Image(RCC)")
-ax2[1].imshow(gray_world(gammaCorrection(redChannelCompnesation(image))))
-ax2[1].set_title("Gray World Image(RCC + GC)")
+ax0[0][0].imshow(image)
+ax0[0][1].imshow(whitePatchAlgorithm(image, 95))
+ax0[0][2].imshow(redChannelCompnesatedImage)
+ax0[1][0].imshow(grayWorldImage)
+ax0[1][1].imshow(gammaCorrection(grayWorldImage, 0.5))
+ax0[1][2].imshow(gammaCorrection(grayWorldImage, 1))
+ax0[2][0].imshow(gammaCorrection(grayWorldImage, 1.5))
+ax0[2][1].imshow(gammaCorrection(grayWorldImage, 2))
+ax0[2][2].imshow(gammaCorrection(grayWorldImage, 2.5))
 
 plt.show()
-
-
